63936efe260b32a8332eb5754c10f64891c728bb
David Stuber <rlzdavid20@gmail.com>
2024-06-13 19:17:15 +0200

    Jobs are showing now, created design

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,5 +1,25 @@
 <x-layout>
     @foreach ($jobs as $job)
-        <div>{{ $job->title }}</div>
+        <x-card class="mb-4">
+            <div class="flex justify-between mb-4">
+                <h2 class="text-lg font-medium">{{ $job->title }}</h2>
+                <div class="text-slate-500">
+                    {{ number_format($job->salary) }}€
+                </div>
+            </div>
+
+            <div class="mb-4 flex justify-between items-center text-sm text-slate-500">
+                <div class="flex space-x-3">
+                    <div>Company Name</div>
+                    <div>{{ $job->location }}</div>
+                </div>
+                <div class="flex space-x-1 text-xs">
+                    <x-tag>{{ Str::ucfirst($job->experience) }}</x-tag>
+                    <x-tag>{{ $job->category }}</x-tag>
+                </div>
+            </div>
+
+            <p class="text-sm text-slate-500">{!! nl2br(e($job->description)) !!}</p>
+        </x-card>
     @endforeach
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use App\Http\Controllers\JobListingController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', fn() => to_route('jobs.index'));
+
 Route::resource('jobs', JobListingController::class)
     ->only(['index']);
