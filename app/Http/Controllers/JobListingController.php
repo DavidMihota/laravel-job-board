1415d64ae56ff25ce69e61a8e9250407194ea735
David Stuber <rlzdavid20@gmail.com>
2024-06-14 15:49:43 +0200

    Search Filters query

--- app/Http/Controllers/JobListingController.php
+++ app/Http/Controllers/JobListingController.php
@@ -12,7 +12,19 @@ class JobListingController extends Controller
      */
     public function index()
     {
-        return view('job.index', ['jobs' => JobListing::all()]);
+        $jobs = JobListing::query();
+
+        $jobs->when(request('search'), function ($query) {
+            $query->where(function ($query) {
+                $query->where('title', 'like', '%'.request('search').'%')
+                    ->orWhere('description', 'like', '%'.request('search').'%');
+            });
+        })->when(request('min_salary'), function ($query) {
+            $query->where('salary', '>=', request('min_salary'));
+        })->when(request('max_salary'), function($query) {
+            $query->where('salary', '<=', request('max_salary'));
+        });
+        return view('job.index', ['jobs' => $jobs->get()]);
     }
 
     /**
